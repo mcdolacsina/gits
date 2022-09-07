fedd8d75e88e84b218fed3404c92ca06789720ff
Enrico P. Chavez <vez3@yahoo.com>
2022-09-07 17:10:55 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import BasicMathView from '../views/BasicMathView.vue'
+import StringAppView from '../views/StringAppView.vue'
+import HomeView from '../views/StringAppView.vue'
+import AxiosDemo from '../views/AxiosDemo.vue'
+
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/stringApp',
+      name: 'StringAppView',
+      component: StringAppView
+    },
+    {
+    path: '/basicMath',
+    name: 'basicMath',
+    component: BasicMathView
+    },
+    {
+    path: '/axiosDemo',
+    name:'axiosDemo',
+    component: AxiosDemo
+    },
+
+  ]
+})
+
+export default router
